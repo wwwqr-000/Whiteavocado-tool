8042638a4fa01078dc9b627c0b52301a4204250d
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2024-07-29 17:46:11 +0300

    New dll integration + added json .hpp for json-based gui application idea

--- main.cpp
+++ main.cpp
@@ -7,28 +7,28 @@
 #include <memory>
 #include <functional>
 
+#include "classes/json.hpp"
 #include "classes/dimensions.hpp"
 #include "classes/frameObj.hpp"
 #include "classes/frames/box.hpp"
 #include "classes/frames/menuItem.hpp"
 #include "classes/frames/menu.hpp"
 
+using json = nlohmann::json;
+
 HMODULE WDll = LoadLibraryExW(L"whiteavocado64.dll", nullptr, 0);
 
 using DR = void (__cdecl*)(int, int, int, int, int, int, int, int, bool);
 using DP = void (__cdecl*)(int, int, int, int, int, bool);
 using MB = void (__cdecl*)(const char*, const char*, const char*, const char*, std::string&);
+using CS = void (__cdecl*)();
 
 DR const drawLine = reinterpret_cast<DR>(GetProcAddress(WDll, "drawLine"));
 DP const drawPixel = reinterpret_cast<DP>(GetProcAddress(WDll, "drawPixel"));
 MB const msgBox = reinterpret_cast<MB>(GetProcAddress(WDll, "msgBox"));
+CS const cls = reinterpret_cast<CS>(GetProcAddress(WDll, "cls"));
 
 std::string msgRes;
-
-void cls() {
-    InvalidateRect(NULL, NULL, TRUE);
-    UpdateWindow(NULL);
-}
 
 int x;
 
