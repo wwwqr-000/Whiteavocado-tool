d4bbf5091364c0bf478d81d92773304262a82b0a
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2024-08-02 22:21:49 +0300

    Added windowFrame movement with cursor

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@ std::vector<std::thread> threads;
 frame windowFrame;
 bool update = true;
 bool active = true;
+bool validDragPos = false;//If the mouse drag is in the title bar of the window.
 HMODULE WDll = LoadLibraryExW(L"whiteavocado64.dll", nullptr, 0);
 //
 
@@ -37,6 +38,7 @@ using GR = void (__cdecl*)(int&, int&);
 using FR = void (__cdecl*)(int, int, int, int, int, int, int);
 using DT = void (__cdecl*)(const char*, int, int, int, int, int, int, int, int, int, int, int);
 using GSN = const char* (__cdecl*)();
+using BL = const int (__cdecl*)(std::string&);
 
 DR const drawLine = reinterpret_cast<DR>(dpcn("drawLine"));
 DP const drawPixel = reinterpret_cast<DP>(dpcn("drawPixel"));
@@ -46,6 +48,8 @@ GR const getScreenResolution = reinterpret_cast<GR>(dpcn("getScreenResolution"))
 FR const fillRect = reinterpret_cast<FR>(dpcn("fillRect"));
 DT const drawTxt = reinterpret_cast<DT>(dpcn("drawTxt"));
 GSN const getSelfName = reinterpret_cast<GSN>(dpcn("getSelfName"));
+BL const buttonListener = reinterpret_cast<BL>(dpcn("buttonListener"));
+GR const getCursorPos = reinterpret_cast<GR>(dpcn("getCursorPos"));
 
 void drawBox(RECT box, point3 rgb) {
     drawLine(box.left, box.top, box.right, box.top, 1, rgb.x_i, rgb.y_i, rgb.z_i, false);
@@ -64,6 +68,32 @@ void drawScreen() {
     drawWindowFrame();
 }
 
+void mouseBLThread() {//Mousebutton listener callback func
+    while (active) {
+        std::string stat;
+        int index = buttonListener(stat);
+        int x, y;
+        getCursorPos(x, y);
+        if (index == 0) {//Left button down
+            if (x >= windowFrame.getBX() && x <= windowFrame.getEX() && y >= windowFrame.getBY() && y <= (windowFrame.getBY() + 22)) {
+                validDragPos = true;
+            }
+        }
+        else if (index == 3) {//Left button up
+            if (validDragPos) {
+                windowFrame.setEX((windowFrame.getEX() - windowFrame.getBX()) + x);
+                windowFrame.setEY((windowFrame.getEY() - windowFrame.getBY()) + y);
+                windowFrame.setBX(x);
+                windowFrame.setBY(y);
+                cls();
+                Sleep(20);
+                cls();
+            }
+            validDragPos = false;
+        }
+    }
+}
+
 void tick() {
     while (active) {
         if (update) {
@@ -75,6 +105,7 @@ void tick() {
 
 int main() {
     windowFrame = frame(10, 10, 710, 510, "Tool");
+    threads.emplace_back([]{ mouseBLThread(); });
     while (active) {
         tick();
     }
